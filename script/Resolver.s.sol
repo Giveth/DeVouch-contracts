79c9b36011a84da08ce1184eeebcf4707fbb2b6c
Amin Latifi <a.latifi.al@gmail.com>
2024-07-29 15:15:36 +0330

    Added Resolver deploy script

---
+++ script/Resolver.s.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Script, console} from "forge-std/Script.sol";
+import {EAS} from "eas-contracts/contracts/EAS.sol";
+import {Upgrades} from "openzeppelin-foundry-upgrades/Upgrades.sol";
+import {DeVouchResolverUpgradeable} from "src/DeVouchResolverUpgradeable.sol";
+
+contract ResolverScript is Script {
+    function setUp() public {}
+
+    function run() public {
+        EAS easContract = EAS(vm.envAddress("EAS_CONTRACT"));
+        address owner = vm.envAddress("OWNER_ADDRESS");
+
+        vm.startBroadcast();
+
+        address proxy = Upgrades.deployTransparentProxy(
+            "DeVouchResolverUpgradeable.sol:DeVouchResolverUpgradeable",
+            owner,
+            abi.encodeCall(DeVouchResolverUpgradeable.initialize, (easContract, 0 ether))
+        );
+
+        console.log("Resolver: %s", proxy);
+        console.log("Implementation: %s", Upgrades.getImplementationAddress(proxy));
+        console.log("Proxy Admin: %s", Upgrades.getAdminAddress(proxy));
+
+        vm.stopBroadcast();
+    }
+}
